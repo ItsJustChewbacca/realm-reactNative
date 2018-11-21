09e0ea102cad9cf3e8f2c48461072aa8dd629a9c
Luke Baziw-Grossmann <luke@metallic.localdomain>
2018-11-21 10:49:06 -0800

    added cloud realm instanc

--- toDO/src/constants.js
+++ toDO/src/constants.js
@@ -1,22 +1,11 @@
-export const Project = {
-  name: "Project",
-  primaryKey: "projectId",
-  properties: {
-    projectId: "string",
-    owner: "string",
-    name: "string",
-    timestamp: "date",
-    items: "Item[]"
-  }
-};
-​
-export const Item = {
-  name: "Item",
-  primaryKey: "itemId",
-  properties: {
-    itemId: "string",
-    body: "string",
-    isDone: "bool",
-    timestamp: "date"
-  }
-};
+// **** Realm Cloud Users:
+// **** Replace MY_INSTANCE_ADDRESS with the hostname of your cloud instance
+// **** e.g., "mycoolapp.us1.cloud.realm.io"
+// ****
+// ****
+// **** ROS On-Premises Users
+// **** Replace the SERVER_URL string with the fully qualified versions of
+// **** address of your ROS server, e.g.: "http://127.0.0.1:9080".
+
+const MY_INSTANCE_ADDRESS = "bmhtest.us1a.cloud.realm.io"; 
+export const SERVER_URL = `https://${MY_INSTANCE_ADDRESS}`;
