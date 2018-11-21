af63810e6e885a7fcab4baaaab3a8af8a92af880
Luke Baziw-Grossmann <luke@metallic.localdomain>
2018-11-21 11:39:41 -0800

    logout or render projects

--- toDO/src/components/LoginForm.js
+++ toDO/src/components/LoginForm.js
@@ -26,61 +26,73 @@ const styles = StyleSheet.create({
 export class LoginForm extends Component {
   state = {};
   componentDidMount() {
-  // Check if we're already authenticated
-  if (Realm.Sync.User.current) {
-    this.onAuthenticated(Realm.Sync.User.current);
-  } else {
-    this.setState({ isModalVisible: true });
+    // Check if we're already authenticated
+    if (Realm.Sync.User.current) {
+      this.onAuthenticated(Realm.Sync.User.current);
+    } else {
+      this.setState({ isModalVisible: true });
+    }
   }
-}
-handleSubmit = async nickname => {
-  try {
-    // Reset any previous errors that might have happened
-    this.setState({ error: undefined });
-    // Attempt to authenticate towards the server
-    const user = await Realm.Sync.User.registerWithProvider(SERVER_URL, {
-      provider: "nickname",
-      providerToken: nickname
+  handleSubmit = async nickname => {
+    try {
+      // Reset any previous errors that might have happened
+      this.setState({ error: undefined });
+      // Attempt to authenticate towards the server
+      const user = await Realm.Sync.User.registerWithProvider(SERVER_URL, {
+        provider: "nickname",
+        providerToken: nickname
+      });
+      // Hide the modal
+      this.setState({ isModalVisible: false });
+      this.onAuthenticated(user);
+    } catch (error) {
+      this.setState({ isModalVisible: true, error });
+    }
+  }
+  onAuthenticated(user) {
+    // Create a configuration to open the default Realm
+    const config = user.createConfiguration({
+      schema: [Project, Item]
     });
-    // Hide the modal
-    this.setState({ isModalVisible: false });
-    this.onAuthenticated(user);
-  } catch (error) {
-    this.setState({ isModalVisible: true, error });
+    // Open the Realm
+    const realm = new Realm(config);
+    // Navigate to the main scene
+    Actions.authenticated({ user, realm });
   }
-}
-onAuthenticated(user) {
-  // Create a configuration to open the default Realm
-  const config = user.createConfiguration({
-    schema: [Project, Item]
-  });
-  // Open the Realm
-  const realm = new Realm(config);
-  // Navigate to the main scene
-  Actions.authenticated({ user, realm });
-}
+
+  onLogout = () => {
+      if (Realm.Sync.User.current) {
+        Realm.Sync.User.current.logout();
+        this.forceUpdate();
+      }
+  };
+  
+  onOpenProjects = () => {
+      if (Realm.Sync.User.current) {
+        this.onAuthenticated(Realm.Sync.User.current);
+      }
+  };
   
   render() {
      // Show the modal if the user is not authenticated
-  const isAuthenticated = !!Realm.Sync.User.current;
+    const isAuthenticated = !!Realm.Sync.User.current;
   
-  return (
-    <View style={styles.container}>
-      <ModalView
-        placeholder="Please Enter a Username"
-        confirmLabel="Login"
-        isModalVisible={!isAuthenticated}
-        handleSubmit={this.handleSubmit}
-        error={this.state.error}
-      />
-      {isAuthenticated && (
-        <View style={styles.buttons}>
-          <Button onPress={this.onLogout}>Logout</Button>
-          <Button onPress={this.onOpenProjects}>Go to projects</Button>
-        </View>
-      )}
-    </View>
-  );
-
+    return (
+      <View style={styles.container}>
+        <ModalView
+          placeholder="Please Enter a Username"
+          confirmLabel="Login"
+          isModalVisible={!isAuthenticated}
+          handleSubmit={this.handleSubmit}
+          error={this.state.error}
+        />
+        {isAuthenticated && (
+          <View style={styles.buttons}>
+            <Button onPress={this.onLogout}>Logout</Button>
+            <Button onPress={this.onOpenProjects}>Go to projects</Button>
+          </View>
+        )}
+      </View>
+    );
   }
 }
