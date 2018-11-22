7c208e9e7504ead018392ad9f9dd1d9a049610fd
Luke Baziw-Grossmann <luke@Lukes-MacBook-Pro.local>
2018-11-22 13:25:33 -0800

    removed try block and put then/catch promise

--- toDO/src/components/LoginForm.js
+++ toDO/src/components/LoginForm.js
@@ -29,43 +29,17 @@ export class LoginForm extends Component {
       this.setState({ isModalVisible: true });
     }
   }
-  render() {
-     // Show the modal if the user is not authenticated
-    const isAuthenticated = !!Realm.Sync.User.current;
-  
-    return (
-      <View style={styles.container}>
-        <ModalView
-          placeholder="Please Enter a Username"
-          confirmLabel="Login"
-          isModalVisible={!isAuthenticated}
-          handleSubmit={this.handleSubmit}
-          error={this.state.error}
-        />
-        {isAuthenticated && (
-          <View style={styles.buttons}>
-            <Button onPress={this.onLogout}>Logout</Button>
-            <Button onPress={this.onOpenProjects}>Go to projects</Button>
-          </View>
-        )}
-      </View>
-    );
-  }
   handleSubmit = async nickname => {
-    try {
       // Reset any previous errors that might have happened
       this.setState({ error: undefined });
       // Attempt to authenticate towards the server
-      const user = await Realm.Sync.User.registerWithProvider(SERVER_URL, {
-        provider: "nickname",
-        providerToken: nickname
-      });
+      const user = await Realm.Sync.User.login(SERVER_URL, 'Luke', '123456').then(user => {
       // Hide the modal
       this.setState({ isModalVisible: false });
       this.onAuthenticated(user);
-    } catch (error) {
+      }).catch(error => {
       this.setState({ isModalVisible: true, error });
-    }
+    })
   }
   onAuthenticated(user) {
     // Create a configuration to open the default Realm
@@ -90,4 +64,26 @@ export class LoginForm extends Component {
         this.onAuthenticated(Realm.Sync.User.current);
       }
   };
+  render() {
+     // Show the modal if the user is not authenticated
+    const isAuthenticated = !!Realm.Sync.User.current;
+  
+    return (
+      <View style={styles.container}>
+        <ModalView
+          placeholder="Please Enter a Username"
+          confirmLabel="Login"
+          isModalVisible={!isAuthenticated}
+          handleSubmit={this.handleSubmit}
+          error={this.state.error}
+        />
+        {isAuthenticated && (
+          <View style={styles.buttons}>
+            <Button onPress={this.onLogout}>Logout</Button>
+            <Button onPress={this.onOpenProjects}>Go to projects</Button>
+          </View>
+        )}
+      </View>
+    );
+  }
 }
